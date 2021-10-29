13a99963b8f9bc169741c21a76800ca05e43da0e
Flaki <git@flaki.hu>
2021-10-29 20:42:51 +0300

    First create-cfp-app release

---
+++ create-cfp-app/README.md
@@ -0,0 +1,9 @@
+# Create CFP-App
+
+An easy way to spin up a [cfp-app](https://www.npmjs.com/package/cfp-app) Call for Proposals form instance. Usage:
+
+```
+yarn create cfp-app
+```
+
+After that, follow the instructions.
---
+++ create-cfp-app/create-cfp-app.js
@@ -0,0 +1,71 @@
+#!/bin/env node
+const { writeFileSync } = require('fs')
+const { resolve } = require('path')
+const { pathToFileURL } = require('url')
+const { prompt } = require('enquirer')
+
+const exampleConfig = pathToFileURL(resolve(__dirname, './example-config.json'))
+const customization = [
+  {
+    type: 'input',
+    name: 'configurationUrl',
+    message: 'Configuration file? (defaults to the example config.)',
+    initial: 'example-config.json',
+    footer: 'This can be either JSON file, local or online, or the URL of a Strapi backend instance (with the cfp plugin installed and configured).'
+  },
+  {
+    type: 'input',
+    name: 'localeDataUrl',
+    message: 'URL of the localization data?',
+    initial: 'https://raw.githubusercontent.com/RustFestEU/localization/main',
+  },
+  {
+    type: 'input',
+    name: 'localeFiles',
+    message: 'L10n filenames to look for?',
+    initial: 'cfp-form.ftl|events.ftl',
+  },
+  {
+    type: 'input',
+    name: 'deployDir',
+    message: 'Deploy dir?',
+    initial: __dirname+'/demo'
+  },
+  {
+    type: 'input',
+    name: 'assetsDir',
+    message: 'Path of the CFP App assets? (JS,CSS)',
+    initial: 'assets'
+  },
+  {
+    type: 'input',
+    name: 'writeIndex',
+    message: 'Create the HTML output? (default: yes)',
+    initial: 'index.html'
+  },
+  {
+    type: 'input',
+    name: 'writeHeaders',
+    meesage: 'Create the HTML header-contents only file? (default: no)',
+    writeHeaders: '',
+  }
+]
+
+if (!process.argv[1].endsWith('/create-cfp-app.js')) {
+  console.error('Please run standalone (e.g.: yarn create cfp-app)')
+  process.exit(1)
+}
+
+(async () => {
+  const cfpApp = await import('cfp-app')
+
+  const options = await prompt(customization)
+  if (options.configurationUrl === 'example-config.json') options.configurationUrl = exampleConfig
+
+  const settings = options.deployDir+'/.settings.json'
+  writeFileSync(settings, JSON.stringify(options))
+  console.log('Settings saved in: '+settings)
+
+  await cfpApp.default(options)
+
+})().then(r => process.exit(0), e => process.exit(console.error(e) ?? 1))
